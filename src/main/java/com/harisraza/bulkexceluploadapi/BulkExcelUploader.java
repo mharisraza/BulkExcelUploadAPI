3a19ac864bbdc7ab250142fffc7f675866553062
Muhammad Haris <wannaheadshot5@gmail.com>
2024-06-25 01:04:56 +0500

    init project

---
+++ src/main/java/com/harisraza/bulkexceluploadapi/BulkExcelUploader.java
@@ -0,0 +1,10 @@
+package com.harisraza.bulkexceluploadapi;
+
+/*
+ * This is main class for BulkExcelUploader.
+ */
+public class BulkExcelUploader {
+    public static void main(String[] args) {
+        System.out.println("BulkExcelUploader works!");
+    }
+}
